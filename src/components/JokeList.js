bb60bf65a2af3dadec793059f12aa7cb0600a9b8
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-02-29 17:22:15 +0100

    Syncing with localStorage

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -10,10 +10,15 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { jokes: [] };
+    this.state = {
+      jokes: JSON.parse(window.localStorage.getItem("jokes")) || "[]"
+    };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    if (this.state.jokes.length === 0) this.getJokes();
+  }
+  async getJokes() {
     let jokes = [];
     while (jokes.length < this.props.numOfJokes) {
       let res = await axios.get("https://icanhazdadjoke.com/", {
@@ -23,6 +28,7 @@ class JokeList extends Component {
     }
 
     this.setState({ jokes: jokes });
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
   }
   handleVote(id, delta) {
     this.setState(curState => ({
